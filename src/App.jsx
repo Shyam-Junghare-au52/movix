6c9561371fad7982cf8dbe608a6745b3be0bf2bc
Shyam-Junghare-au50 <shyamjunghare56@gmail.com>
2023-07-02 15:54:41 +0530

    App Modified

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ function App() {
                 <Route path="*" element={<PageNotFound />} />
             </Routes>
             <Footer />
-        </BrowserRouter> 
+        </BrowserRouter>
     );
 }
 
